688b9d9fd188cb11c71af9c96a3f89cda23ddef3
MaxBogomol <maxglotov159@gmail.com>
2026-02-24 00:02:54 +0500

    flowawwawawa

--- src/main/java/mod/maxbogomol/purrfect/client/model/curio/FlowerWreathModel.java
+++ src/main/java/mod/maxbogomol/purrfect/client/model/curio/FlowerWreathModel.java
@@ -27,6 +27,19 @@ public class FlowerWreathModel extends HumanoidModel {
         PartDefinition model = head.addOrReplaceChild("model", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.55F)), PartPose.ZERO);
         PartDefinition model1 = head.addOrReplaceChild("model1", CubeListBuilder.create().texOffs(32, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.85F)), PartPose.ZERO);
 
+        PartDefinition model2 = model.addOrReplaceChild("model2", CubeListBuilder.create().texOffs(-10, 16).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 0.0F, 10.0F, new CubeDeformation(0.0F)),  PartPose.offsetAndRotation(0.0f, -8.2f, -4.8f, (float) Math.toRadians(55f),0.0f, 0.0f));
+        PartDefinition model3 = model.addOrReplaceChild("model3", CubeListBuilder.create().texOffs(-10, 16).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 0.0F, 10.0F, new CubeDeformation(0.0F)),  PartPose.offsetAndRotation(0.0f, -8.2f, 4.8f, (float) Math.toRadians(59f), (float) Math.toRadians(177f), 0.0f));
+        PartDefinition model4 = model.addOrReplaceChild("model4", CubeListBuilder.create().texOffs(-10, 16).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 0.0F, 10.0F, new CubeDeformation(0.0F)),  PartPose.offsetAndRotation(-4.8f, -8.2f, 0.0f, (float) Math.toRadians(47f), (float) Math.toRadians(91f), 0.0f));
+        PartDefinition model5 = model.addOrReplaceChild("model5", CubeListBuilder.create().texOffs(-10, 16).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 0.0F, 10.0F, new CubeDeformation(0.0F)),  PartPose.offsetAndRotation(4.8f, -8.2f, 0.0f, (float) Math.toRadians(38f), (float) Math.toRadians(-86f), 0.0f));
+
+        PartDefinition model6 = model.addOrReplaceChild("model6", CubeListBuilder.create().texOffs(10, 16).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 0.0F, 10.0F, new CubeDeformation(0.0F)),  PartPose.offsetAndRotation(-4.25f, -10.2f, 0.0f, (float) Math.toRadians(45f + 30f), (float) Math.toRadians(91f), (float) Math.toRadians(10f)));
+        PartDefinition model7 = model.addOrReplaceChild("model7", CubeListBuilder.create().texOffs(10, 16).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 0.0F, 10.0F, new CubeDeformation(0.0F)),  PartPose.offsetAndRotation(4.25f, -10.2f, 0.0f, (float) Math.toRadians(47f + 30f), (float) Math.toRadians(-86f), (float) Math.toRadians(-10f)));
+
+        PartDefinition model8 = model.addOrReplaceChild("model8", CubeListBuilder.create().texOffs(30, 16).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 0.0F, 10.0F, new CubeDeformation(0.0F)),  PartPose.offsetAndRotation(0.0f, -10.2f, -4.8f, (float) Math.toRadians(55f + 30f),0.0f, 0.0f));
+        PartDefinition model9 = model.addOrReplaceChild("model9", CubeListBuilder.create().texOffs(30, 16).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 0.0F, 10.0F, new CubeDeformation(0.0F)),  PartPose.offsetAndRotation(0.0f, -10.2f, 4.8f, (float) Math.toRadians(59f + 30f), (float) Math.toRadians(177f), 0.0f));
+        PartDefinition model10 = model.addOrReplaceChild("model10", CubeListBuilder.create().texOffs(30, 16).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 0.0F, 10.0F, new CubeDeformation(0.0F)),  PartPose.offsetAndRotation(-4.4f, -10.2f, 0.0f, (float) Math.toRadians(47f + 30f), (float) Math.toRadians(91f), 0.0f));
+        PartDefinition model11 = model.addOrReplaceChild("model11", CubeListBuilder.create().texOffs(30, 16).addBox(-5.0F, 0.0F, -5.0F, 10.0F, 0.0F, 10.0F, new CubeDeformation(0.0F)),  PartPose.offsetAndRotation(4.4f, -10.2f, 0.0f, (float) Math.toRadians(38f + 30f), (float) Math.toRadians(-86f), 0.0f));
+
         return LayerDefinition.create(mesh, 64, 64);
     }
 
